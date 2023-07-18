84ad3422507c34510d2ecfc51e0154873be22b8e
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-18 19:16:36 +0300

    Add project files.

---
+++ lab5ex2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care sa verifice daca un sir de caractere citit de la tastatura este sau nu palindrom.*/
+
+
+            Console.WriteLine("Introduceti sirul de caractere: ");
+            string text = (Console.ReadLine());
+
+            string textInversat="";
+
+            for (int i= text.Length-1;i>=0;i--)
+            {
+                textInversat = textInversat + text[i];
+            }
+
+            if (text == textInversat)
+            {
+            Console.WriteLine("\n"+ $"Textul {text} este palindrom. ");
+            }
+            else 
+            {
+             Console.WriteLine("\n"+ $"Textul {text} nu este palindrom. ");
+            }
+
+        }
+
+    }
+
+}
+
+
